99a9d2ae015169686ec815c3ab0834af48005d1f
Walter-Vigiambiental <walteralves.vigiambientalmoc@gmail.com>
2025-07-29 10:28:59 -0300

    Update formulario_hipoclorito.py

--- formulario_hipoclorito.py
+++ formulario_hipoclorito.py
@@ -4,10 +4,10 @@ import streamlit as st
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("logo_hipoclorito2.png", width=180)
+    st.image("logo_hipoclorito2.png", width=100)
 
 with col2:
-    st.image("logo_hipoclorito3.png", width=180)
+    st.image("logo_hipoclorito3.png", width=100)
 
 import pandas as pd
 from datetime import datetime
